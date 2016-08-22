7ffe8f23fca1021ef0b9223537f1cb3e96d4264e
leandrogoncalves <contato.leandrogoncalves@gmail.com>
2016-08-22 16:59:39 -0300

    add exception class

---
+++ src/exceptions/NullException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace leandrogoncalves\exceptions;
+
+class NullException extends \Exception
+{
+    public  function __construct($message, $code, Exception $previous)
+    {
+        parent::__construct($message, $code, $previous);
+        return "O parametro nao pode ser nulo. Detalhes " . parent::getMessage();
+    }
+}
